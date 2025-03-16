ef118def1078495f22ee3f362b36e3605681a346
Djbr-mren <mengchar@uci.edu>
2025-03-16 05:23:17 +0000

    Should fix the printing format

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -39,7 +39,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "Total cost is " << total << "\n";  
+        cout << "\nTotal cost is " << total << "\n";  
     } else {
         for (size_t i = 0; i < path.size(); ++i) {
             cout << path[i];
