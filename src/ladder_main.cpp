a650be08b53d3f48a20015d0f6e547e5ab95e682
Djbr-mren <mengchar@uci.edu>
2025-03-16 04:21:48 +0000

    working on dijkstra first

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,26 +1,26 @@
-#include "ladder.h"
-#include <iostream>
-#include <set>
+// #include "ladder.h"
+// #include <iostream>
+// #include <set>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    set<string> word_list;
-    load_words(word_list, "words.txt");
+// int main() {
+//     set<string> word_list;
+//     load_words(word_list, "words.txt");
 
-    string start_word, end_word;
-    cout << "Enter start word: ";
-    cin >> start_word;
-    cout << "Enter end word: ";
-    cin >> end_word;
+//     string start_word, end_word;
+//     cout << "Enter start word: ";
+//     cin >> start_word;
+//     cout << "Enter end word: ";
+//     cin >> end_word;
 
-    if (start_word == end_word) {
-        cerr << "Error: Start and end words must be different!" << endl;
-        return 1;
-    }
+//     if (start_word == end_word) {
+//         cerr << "Error: Start and end words must be different!" << endl;
+//         return 1;
+//     }
 
-    vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
-    print_word_ladder(ladder);
+//     vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
+//     print_word_ladder(ladder);
 
-    return 0;
-}
+//     return 0;
+// }
